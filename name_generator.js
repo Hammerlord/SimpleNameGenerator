7229b8e03cce89007a3ed5c4cebd1524f1b5503f
Hammerlord <nepharus@gmail.com>
2020-08-02 13:38:30 -0700

    Minor refactor

--- name_generator.js
+++ name_generator.js
@@ -3,9 +3,9 @@ const REDIN = {
 	maxSyllables: 3,
 	minSyllables: 2,
 	syllables: ['mal', 'run', 'ae', 'kor', 'ith', 'tok', 'ro', 'sha', 'thro', 'storn', 'ja', 'bal',
-	'nha', 'han', 'on', 'no', 'os', 'or', 'im', 'la', 'wa', 'yal', 'fii', 'olb', 'arn', 'gan',
-	'ul', 'lok', 'krol', 'umn', 'sek', 'rha', 'rho', 'nu', 'kun', 'ber', 'nol', 'ish', 'an',
-	'rak', 'to', 'kro', 'aul', 'rae', 'li', 'y', 'sho', 'vael', 'oak', 'var', 'ha']
+		'nha', 'han', 'on', 'no', 'os', 'or', 'im', 'la', 'wa', 'yal', 'fii', 'olb', 'arn', 'gan',
+		'ul', 'lok', 'krol', 'umn', 'sek', 'rha', 'rho', 'nu', 'kun', 'ber', 'nol', 'ish', 'an',
+		'rak', 'to', 'kro', 'aul', 'rae', 'li', 'y', 'sho', 'vael', 'oak', 'var', 'ha']
 };
 
 const TIGRUN = {
@@ -13,10 +13,10 @@ const TIGRUN = {
 	maxSyllables: 3,
 	minSyllables: 2,
 	syllables: ['ga', 'wa', 'yi', 'sho', 'ku', 'renn', 'ya', 'ka', 'ro', 'lun', 'fei', 'li', 'bai',
-	'hu', 'kan', 'yu', 'e', 'shi', 'wa', 'ir', 'ha', 'rai', 'rhi', 'al', 'won', 'ta',
-	'miu', 'ko', 'fun', 'tai', 'tan', 'zau', 'hom', 'ang', 'ri', 'hwa', 'jun', 'ban', 'ba',
-	'gunn', 'fu', 'dai', 'ji', 'toh', 'sei', 'o', 'yuu', 'su', 'sai', 'hii', 'yu', 'jae',
-	'mao', 'jang', 'na']
+		'hu', 'kan', 'yu', 'e', 'shi', 'wa', 'ir', 'ha', 'rai', 'rhi', 'al', 'won', 'ta',
+		'miu', 'ko', 'fun', 'tai', 'tan', 'zau', 'hom', 'ang', 'ri', 'hwa', 'jun', 'ban', 'ba',
+		'gunn', 'fu', 'dai', 'ji', 'toh', 'sei', 'o', 'yuu', 'su', 'sai', 'hii', 'yu', 'jae',
+		'mao', 'jang', 'na']
 };
 
 const UMRIN = {
@@ -24,9 +24,9 @@ const UMRIN = {
 	maxSyllables: 3,
 	minSyllables: 2,
 	syllables: ['xir', 'sei', 'xia', 'nan', 'um', 'xis', 'rix', 'zen', 'ri', 'sa', 'in', 'te', 'za', 'zan',
-	'ix', 'im', 'en', 'iil', 'an', 'or', 'sun', 'ai', 'zue', 'ban', 'gi', 'ten', 'rat', 'taz',
-	'ril', 'ez', 'nas', 'nen', 'ci', 'cia', 'tan', 'ke', 'ser', 'ti', 'set', 'zer', 'val',
-	'jin', 'xu', 'xen', 'lu', 'ma', 'el', 'ish', 'zel', 'zet', 'ys', 'cy']
+		'ix', 'im', 'en', 'iil', 'an', 'or', 'sun', 'ai', 'zue', 'ban', 'gi', 'ten', 'rat', 'taz',
+		'ril', 'ez', 'nas', 'nen', 'ci', 'cia', 'tan', 'ke', 'ser', 'ti', 'set', 'zer', 'val',
+		'jin', 'xu', 'xen', 'lu', 'ma', 'el', 'ish', 'zel', 'zet', 'ys', 'cy']
 };
 
 const RIKELRIN = {
@@ -34,9 +34,9 @@ const RIKELRIN = {
 	maxSyllables: 3,
 	minSyllables: 2,
 	syllables: ['as', 'tha', 'ros', 'ta', 'yv', 'od', 'gro', 'mul', 'da', 'ga', 'ron', 'gor', 'nov',
-	'va', 'nos', 'nod', 'kor', 'an', 'um', 'dra', 'ath', 'born', 'kar', 'olb', 'aun', 'lod', 'ba',
-	'veng', 'rik', 'rol', 'naz', 'ung', 'na', 'tev', 'vin', 'ul', 'tov', 'ata', 'tro', 'yev', 'zha', 'zho',
-	'bor', 'nar']
+		'va', 'nos', 'nod', 'kor', 'an', 'um', 'dra', 'ath', 'born', 'kar', 'olb', 'aun', 'lod', 'ba',
+		'veng', 'rik', 'rol', 'naz', 'ung', 'na', 'tev', 'vin', 'ul', 'tov', 'ata', 'tro', 'yev', 'zha', 'zho',
+		'bor', 'nar']
 };
 
 const RYAS = {
@@ -44,9 +44,9 @@ const RYAS = {
 	maxSyllables: 3,
 	minSyllables: 2,
 	syllables: ['kax', 'zi', 'io', 'nyx', 'ky', 'var', 'fer', 'ze', 'den', 'nar', 'mar', 'rho', 'syl', 'zer',
-	'sol', 'set', 'yos', 'nia', 'ia', 'nae', 'a', 'xil', 'ind', 'va', 'ar', 'naz', 'ir', 'nem', 'ven',
-	'rys', 'ari', 'an', 'em', 'en', 'kin', 'rhy', 'to', 'zet', 'gal', 'kand', 'nar', 'seo', 'ies',
-	'cas', 'zha', 'ius', 'stra', 'ian', 'san', 'dara', 'sef', 'eve', 'tep']
+		'sol', 'set', 'yos', 'nia', 'ia', 'nae', 'a', 'xil', 'ind', 'va', 'ar', 'naz', 'ir', 'nem', 'ven',
+		'rys', 'ari', 'an', 'em', 'en', 'kin', 'rhy', 'to', 'zet', 'gal', 'kand', 'nar', 'seo', 'ies',
+		'cas', 'zha', 'ius', 'stra', 'ian', 'san', 'dara', 'sef', 'eve', 'tep']
 };
 
 const HIGHLANDER = {
@@ -54,25 +54,25 @@ const HIGHLANDER = {
 	maxSyllables: 2,
 	minSyllables: 1,
 	syllables: ['kur', 'rek', 'gen', 'ban', 'jan', 'lan', 'ul', 'ric', 'jorn', 'donn', 'nal', 'thar',
-	'orn', 'gor', 'kor', 'zan', 'sab', 'el', 'in', 'os', 'ro', 'reth', 'toh', 'rahl',
-	'roth', 'vol', 'rath', 'zen', 'ren', 'dol', 'aen', 'ind', 'rha', 'thor', 'jol', 'rab',
-	'aul', 'ta', 'bar', 'ad', 'den', 'wen', 'ma', 'rod', 'dath', 'if', 'thek', 'dar',
-	'nos', 'dra', 'du', 'yn', 'ya', 'gar', 'ae', 'stro', 'rit', 'sor', 'jen', 'auk', 'ran',
-	'stom', 'rom']
+		'orn', 'gor', 'kor', 'zan', 'sab', 'el', 'in', 'os', 'ro', 'reth', 'toh', 'rahl',
+		'roth', 'vol', 'rath', 'zen', 'ren', 'dol', 'aen', 'ind', 'rha', 'thor', 'jol', 'rab',
+		'aul', 'ta', 'bar', 'ad', 'den', 'wen', 'ma', 'rod', 'dath', 'if', 'thek', 'dar',
+		'nos', 'dra', 'du', 'yn', 'ya', 'gar', 'ae', 'stro', 'rit', 'sor', 'jen', 'auk', 'ran',
+		'stom', 'rom']
 };
 
 const LOWLANDER = {
 	label: 'Lowlander',
 	maxSyllables: 2,
 	minSyllables: 1,
-	syllables:  ['ed', 'wyn', 'ric', 'jon', 'ald', 'gen', 'ger', 'mel', 'da', 'in', 'mi', 'as', 'don', 'lis',
-                 'vin', 'etta', 'ton', 'kas', 'stan', 'nic', 'hen', 'ches', 'lin', 'va', 'ner', 'gib', 'son',
-                 'ma', 'ann', 'ren', 'din', 'gor', 'her', 'leo', 'len', 'dal', 'worth', 'stron', 'gard',
-                 'red', 'wend', 'shel', 'ome', 'cyn', 'luc', 'ban', 'jo', 'well', 'cor', 'seth', 'ser', 'nis',
-                 'al', 'jay', 'ai', 'dy', 'tan', 'roth', 'cern', 'wald', 'ven', 'grey', 'bor', 'es', 'ler',
-                 'ster', 'zel', 'dan', 'ole', 'a', 'by', 'ard', 'holl', 'and', 'way', 'clyn', 'wan', 'os', 'brad', 
-                 'wor', 'bert', 'ham', 'lia', 'del', 'bran', 'tram', 'ber', 'mer', 'som', 'beck', 'ian', 'vand', 'ol', 'teo',
-                 'ain', 'tov', 'han', 'lock', 'grimm', 'wyrd', 'ash', 'stan', 'stein']
+	syllables: ['ed', 'wyn', 'ric', 'jon', 'ald', 'gen', 'ger', 'mel', 'da', 'in', 'mi', 'as', 'don', 'lis',
+		'vin', 'etta', 'ton', 'kas', 'stan', 'nic', 'hen', 'ches', 'lin', 'va', 'ner', 'gib', 'son',
+		'ma', 'ann', 'ren', 'din', 'gor', 'her', 'leo', 'len', 'dal', 'worth', 'stron', 'gard',
+		'red', 'wend', 'shel', 'ome', 'cyn', 'luc', 'ban', 'jo', 'well', 'cor', 'seth', 'ser', 'nis',
+		'al', 'jay', 'ai', 'dy', 'tan', 'roth', 'cern', 'wald', 'ven', 'grey', 'bor', 'es', 'ler',
+		'ster', 'zel', 'dan', 'ole', 'a', 'by', 'ard', 'holl', 'and', 'way', 'clyn', 'wan', 'os', 'brad',
+		'wor', 'bert', 'ham', 'lia', 'del', 'bran', 'tram', 'ber', 'mer', 'som', 'beck', 'ian', 'vand', 'ol', 'teo',
+		'ain', 'tov', 'han', 'lock', 'grimm', 'wyrd', 'ash', 'stan', 'stein']
 };
 
 const speciesMap = {
@@ -85,10 +85,26 @@ const speciesMap = {
 	redin: REDIN,
 };
 
-const FILTER = ['ass', 'asian', 'india', 'lol', 'anal', 'danger', 'kinky', 'mister', 
-'strong', 'greed', 'nerdy', 'renal', 'bored', 'hair', 'satan'];
+// Just a quick way to remove unwanted words...
+const FILTER = [
+	'ass',
+	'asian',
+	'india',
+	'lol',
+	'anal',
+	'danger',
+	'kinky',
+	'mister',
+	'strong',
+	'greed',
+	'nerdy',
+	'renal',
+	'bored',
+	'hair',
+	'satan'
+];
 
-window.onload = function() {
+window.onload = function () {
 	const selectionBox = document.getElementById('species');
 	for (const key in speciesMap) {
 		const option = document.createElement('option');
@@ -99,9 +115,8 @@ window.onload = function() {
 	selectionBox.selectedIndex = 0;
 	selectionBox.focus();
 	const textBox = document.querySelector('.results-container textarea');
-	textBox.addEventListener('focus', () => textBox.select());
-	const generateButton = document.querySelector('button');
-	generateButton.addEventListener('click', () => generate());
+	textBox.addEventListener('focus', textBox.select);
+	document.querySelector('button').addEventListener('click', generate);
 	generate();
 };
 
@@ -141,10 +156,10 @@ function changeResultsTitle(species) {
 
 function makeNameNode(name) {
 
-	const recordName = (name) => {
+	const recordName = () => {
 		const textBox = document.querySelector('.results-container textarea');
 		const content = textBox.value.trim();
-		if (content === '') {
+		if (!content) {
 			textBox.value = name;
 		} else {
 			textBox.value = content + "\n" + name;
@@ -155,7 +170,7 @@ function makeNameNode(name) {
 	node.textContent = name;
 	const icon = document.createElement('span');
 	icon.textContent = '›';
-	icon.addEventListener('click', () => recordName(name));
+	icon.addEventListener('click', recordName);
 	node.appendChild(icon);
 	return node;
 }
